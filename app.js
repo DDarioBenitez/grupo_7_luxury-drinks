15c2bf1cfb655713d05e7f8abe52a7564931187e
AntonelaDuckwen <antoduckwen@gmail.com>
2024-01-24 17:19:25 -0300

    estructura

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express();
+
+app.use(express.static('public'));
+
+const port= 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+
+app.listen(port,() => console.log(`http://localhost:${port}`))
