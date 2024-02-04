ade9174ba125184a7fe3771737f8288556f95d3a
DDarioBenitez <reedq2.alt@gmail.com>
2024-02-04 00:01:36 -0300

    Modificacion tablet y desktop

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const app = express();
 
 app.use(express.static('public'));
 
-const port= 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.get('/detail', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register'))
+})
 
 
-app.listen(port,() => console.log(`http://localhost:${port}`))
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
