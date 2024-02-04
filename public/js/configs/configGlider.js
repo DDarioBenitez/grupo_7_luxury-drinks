99e098d2c345cdae099fc683fcb8afd6cb0e8aa1
lucasfasanella00 <edittryhard@outlook.com>
2024-02-04 01:42:02 -0300

    sliders de cards finalizados

---
+++ public/js/configs/configGlider.js
@@ -0,0 +1,239 @@
+
+//Slider Cards
+new Glider(document.querySelector('.glider'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider2'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev2',
+    next: '.glider-next2'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider3'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev3',
+    next: '.glider-next3'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider4'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev4',
+    next: '.glider-next4'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider4'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev4',
+    next: '.glider-next4'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider5'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev5',
+    next: '.glider-next5'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
+
+new Glider(document.querySelector('.glider6'), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev6',
+    next: '.glider-next6'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: 'auto',
+        itemWidth: 600,
+        duration: 0.25
+      }
+    }, {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 700,
+        duration: 0.25
+      }
+    }
+  ]
+});
