100b372773385d136e618bafdc17e24fb37829d7
lucasfasanella00 <edittryhard@outlook.com>
2024-03-03 18:14:10 -0300

    cambios hechos

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,6 +2,5 @@ module.exports = {
     createProduct: require("./createProduct.controller"),
     updateProduct: require ("./updateProduct.controller"),
     listProducts: require("./listProducts.controller"),
-    deleteProduct: require("./deleteProduct.controller"),
-    removeProduct: require("./removeProduct.controller")
+    deleteProduct: require("./deleteProduct.controller")
 }
