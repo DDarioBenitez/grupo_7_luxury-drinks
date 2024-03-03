908fbd6807c91fc6d2c3b3c6685e9e7848fb99ec
lucasfasanella00 <edittryhard@outlook.com>
2024-03-03 20:25:09 -0300

    cambios de organizacion de carpetas y archivos

--- controllers/admin/listProducts.controller.js
+++ controllers/admin/listProducts.controller.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const products = require(path.join(__dirname, '../../data'));
+const products = require(path.join(__dirname, '../../data/products.json'));
 module.exports = (req, res) => {
     res.render("admin/listProducts", {
         products
