fb4a6b3bbcbd8bf7fec9dc10618d056beed4266b
lucasfasanella00 <edittryhard@outlook.com>
2024-03-10 13:23:50 -0300

    cambios descartados

--- controllers/admin/listProducts.controller.js
+++ controllers/admin/listProducts.controller.js
@@ -1,7 +1,6 @@
-const path = require('path');
-const products = require(path.join(__dirname, '../../data/products.json'));
-module.exports = (req, res) => {
-    res.render("admin/listProducts", {
-        products
-    });
+const { loadData } = require("../../data")
+
+module.exports = (req, res)=>{
+    const products = loadData();
+    res.render("admin/listProducts",{products})
 }
