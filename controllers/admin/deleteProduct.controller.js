fa373a8386490fbf3d4316d0ad59762b3c4330e3
DDarioBenitez <reedq2.alt@gmail.com>
2024-03-14 02:05:25 -0300

    fixed

--- controllers/admin/deleteProduct.controller.js
+++ controllers/admin/deleteProduct.controller.js
@@ -2,21 +2,21 @@ const { loadData, saveData } = require("../../data");
 const path = require("path")
 const fs = require("fs")
 
-module.exports=(req,res)=>{
-    const {id} = req.params;
+module.exports = (req, res) => {
+    const { id } = req.params;
     const products = loadData();
-    
+
     const productsLessOne = products.filter(p => p.id !== +id)
     const productDestroy = products.find(p => p.id == +id)
 
     const pathFile = path.join(__dirname, "../../../public/images/products" + productDestroy.image);
 
     const existFile = fs.existsSync(pathFile)
-    if(existFile){
+    if (existFile) {
         fs.unlinkSync(pathFile)
     }
 
     saveData(productsLessOne)
 
-    res.redirect('admin/lista-de-productos')
+    res.redirect('/admin/lista-de-productos')
 }
