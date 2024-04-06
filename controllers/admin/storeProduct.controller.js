14417feedf8b2689da7ea8e87c31a69f51a35b30
AntonelaDuckwen <antoduckwen@gmail.com>
2024-04-06 17:14:25 -0300

    cambios

--- controllers/admin/storeProduct.controller.js
+++ controllers/admin/storeProduct.controller.js
@@ -1,9 +1,11 @@
 const {loadData, saveData} = require("../../data")
 
 module.exports = (req, res) => {       
-   const products = loadData()
+   
    const { name, description, price, stock, image, category } = req.body;
-    const newId = products [products.length - 1].id + 1
+   const products = loadData()
+
+   const newId = products [products.length - 1].id + 1
 
  const newProduct = {
     id: newId,
