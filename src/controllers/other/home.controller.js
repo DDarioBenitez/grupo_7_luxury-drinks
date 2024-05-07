9e4589fdbef606be4451a38d124adfc3d07f147a
DDarioBenitez <reedq2.alt@gmail.com>
2024-05-07 01:11:29 -0300

    fix home

--- src/controllers/other/home.controller.js
+++ src/controllers/other/home.controller.js
@@ -1,14 +1,16 @@
 const productService = require("../services/productService");
+const categoryService = require("../services/categoryService");
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = (req, res) => {
     const products = productService.findAllProducts();
-    const productsVinos = products.filter(p => p.category.toLowerCase() === 'vinos')
-    const productsEspumantes = products.filter(p => p.category.toLowerCase() === 'espumantes')
-    const productsWhiskys = products.filter(p => p.category.toLowerCase() === 'whiskys')
-    const productsLicores = products.filter(p => p.category.toLowerCase() === 'licores')
-    const productsOtros = products.filter(p => p.category.toLowerCase() === 'otros')
-    const productsPopulares = products.filter(p => p.category.toLowerCase() === 'populares')
+    const categorys = categoryService.getCategorys();
+    const productsVinos = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'vinos').id)
+    const productsEspumantes = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'espumantes').id)
+    const productsWhiskys = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'whiskys').id)
+    const productsLicores = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'licores').id)
+    const productsOtros = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'otros').id)
+    const productsPopulares = products.filter(p => p.category_id === categorys.find(category.name.toLowerCase() === 'populares').id)
     res.render("other/home", {
         productsVinos,
         productsEspumantes,
