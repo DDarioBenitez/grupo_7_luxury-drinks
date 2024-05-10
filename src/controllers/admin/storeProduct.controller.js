42fb218c3e09bdd5f1a67762b34480b08674c74a
MathiasSaravia <maycolbeach@gmail.com>
2024-05-10 19:37:29 -0300

    error

--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -3,7 +3,7 @@ const db = require("../../database/models")
 module.exports = (req, res) => {       
    const {title,price,discount,description,category} = req.body;
    const image = req.file
-
+res.send(req.body)
    db.product.create({
       title: title.trim(),
       price: +price,
