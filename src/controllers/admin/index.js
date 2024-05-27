36af5290311fbf3081cc1e8b5da0ba247ba2d781
MathiasSaravia <maycolbeach@gmail.com>
2024-05-27 20:49:26 -0300

    carrito de compras boceto

--- src/controllers/admin/index.js
+++ src/controllers/admin/index.js
@@ -4,5 +4,6 @@ module.exports = {
     updateProduct: require ("./updateProduct.controller"),
     listProducts: require("./listProducts.controller"),
     deleteProduct: require("./deleteProduct.controller"),
-    storeProduct: require ("./storeProduct.controller")
+    storeProduct: require ("./storeProduct.controller"),
+    orderProduct: require("./orderProduct.controller")
 }
