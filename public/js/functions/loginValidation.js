28c718e874b08aee7c9b235c6849ee325252e86c
MathiasSaravia <maycolbeach@gmail.com>
2024-05-30 20:13:18 -0300

    login validation

--- public/js/functions/loginValidation.js
+++ public/js/functions/loginValidation.js
@@ -1,3 +1,34 @@
+
+const errorPassword = document.querySelector("#err-password");
+const errorEmail = document.querySelector(".err-email");
+//EMAIL
+const inputEmail = document.querySelector("[name='email']")
+window.addEventListener("load", () => {   
+    inputEmail.addEventListener("blur",function(){
+        const value = this.value.trim();
+
+        switch(true){
+
+            case value.length === 0:
+                errorEmail.innerHTML = "El campo no puede estar vacío";
+            this.classList.add("is-invalid");
+            break;
+
+            case !isNotEmail.test(value):
+                errorEmail.innerHTML = "Formato no valido";
+            this.classList.add("is-invalid");
+            break;
+
+            default:
+                errorEmail.innerHTML = null;
+            this.classList.remove("is-invalid");
+            this.classList.add("is-valid");
+            break;
+        }
+    })
+})
+
+//PASSWORD
 window.addEventListener("load",() => {
     const inputPass = document.querySelector("#password");
     const btnEye = document.querySelector("#btn-eye");
@@ -13,5 +44,26 @@ window.addEventListener("load",() => {
         }
         iconEye.classList.toggle("fa-eye-slash");
         iconEye.classList.toggle("fa-eye");
-    })
+    });
 });
+
+const inputPassword = document.querySelector("[name='password']");
+window.addEventListener("load", () => {
+    inputPassword.addEventListener("blur",function(){
+        const value = this.value.trim();
+        const errPassword = document.querySelector(".err-password");
+
+        switch(true){
+            case value.length === 0:
+            errPassword.innerHTML = "El campo no puede estar vacío";
+            this.classList.add("is-invalid");
+            break;
+
+            default:
+            errPassword.innerHTML = null;
+            this.classList.remove("is-invalid");
+            this.classList.add("is-valid");
+            break;
+    }
+ })
+})
--- src/middlewares/validations/auth.validation.js
+++ src/middlewares/validations/auth.validation.js
@@ -1,12 +1,13 @@
 const { body } = require("express-validator");
 const { loadData } = require("../../database");
 const { compareSync } = require("bcryptjs");
+const db = require("../../database/models");
 
 const fielMailDefault = body("email")
 .notEmpty().withMessage("Campo requerido").bail()
 .isEmail().withMessage("formato no valido").bail()
 
-const fieldMail =fielMailDefault
+const fieldMail = fielMailDefault
 .custom((value ,{req})=>{
     const users = loadData("users")
     const existUser = users.find(u => u.email === value.trim())
@@ -21,15 +22,21 @@ const fielPasswordRegister = body("password")
 .notEmpty().withMessage("Campo requerido").bail()
 .isLength({min:8,max:16}).withMessage("Longitud invalida").bail()
 
-const fieldMailLogin = body("email")
-.custom((value,{req})=>{
-    const users = loadData("users")
-    const userFind = users.find(u => u.email === email.toLowerCase());
-    if (!userFind) {
-        throw new Error("No existe un usuario con este email")
-    } 
-    return true;  
-})
+const loginEmail = body("emailLogin")
+    .notEmpty().withMessage("Campo requerido").bail()
+    .isEmail().withMessage("Formato invalido").bail()
+    .custom(async (value, { req }) => {
+        try {
+            const userFind = await db.user.findAll({
+                where: { email: value.trim() }
+            })
+            if (!userFind.length) {
+                throw new Error("El email ingresado no está registrado!")
+            }
+        } catch (error) {
+            throw error
+        }
+    });
 
 const fieldPassword = body("password")
 .notEmpty().withMessage("Campo requerido").bail()
@@ -45,5 +52,5 @@ const fieldPassword = body("password")
 
 module.exports = {
     registerValidation:[fieldMail,fielPasswordRegister],
-    LoginValidation:[fieldMailLogin,fieldPassword]
+    LoginValidation:[loginEmail,fieldPassword]
 }
