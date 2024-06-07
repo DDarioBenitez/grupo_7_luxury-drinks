dc300afd9e35b6a0e08d32dfcea69c2c24a988fe
MathiasSaravia <maycolbeach@gmail.com>
2024-06-07 19:33:31 -0300

    fix err login user

--- src/controllers/auth/loginProcess.controller.js
+++ src/controllers/auth/loginProcess.controller.js
@@ -1,48 +1,41 @@
-const bcrypt = require("bcryptjs");
-const { validationResult } = require("express-validator");
-const db = require("../../database/models");
+const db = require('../../database/models');
+const { validationResult } = require("express-validator")
 
-module.exports = (req, res) => {
-    const errors = validationResult(req);
+module.exports = async (req, res) => {
+   try {
+      const errors = validationResult(req)
 
-    if (!errors.isEmpty()) {
-        const { email, password, remember } = req.body;
+      if (errors.isEmpty()) {
+         const { remember } = req.body;
+         const userFind = await db.user.findOne({
+            where: { email: req.body.email.toLowerCase() },
+            attributes: {
+               exclude: ['password']
+            },
+         })
+   
+         const { id, name, email, roleId, avatar } = userFind;
+         
+   
+         req.session.userLogin = {
+            id,
+            name,
+            email,
+            roleId,
+            avatar
+         };
+   
+         if (remember) res.cookie("userLogin", req.session.userLogin, { maxAge: (60000 * 10 ) * 6})
+   
+         return res.redirect("/");
+      } else {
+         throw errors
+      }
 
-        db.user.findOne({
-            where: {
-                email,
-            }
-        }).then((user) => {
-
-            if (!email) {
-                return res.send("debe mandar un email")
-            }
-
-            if (!user) return res.send("El usuario no existe");
-
-            const isPasswordValid = bcrypt.compareSync(password, user?.password);
-
-            if (!isPasswordValid) {
-                return res.send("Contraseña invalida")
-            }
-
-            const { name, surname, rol, avatar, id } = user
-            req.session.userLogin = {
-                id,
-                name,
-                surname,
-                email,
-                rol,
-                avatar,
-            };
-
-            if (remember) {
-                res.cookie("userLogin", req.session.userLogin, { maxAage: 5000 })
-            }
-
-            res.redirect("/")
-            return
-        })
-    }
-}
-
+   } catch (errors) {
+      res.render("./auth/login", {
+         old: req.body,
+         errorsLog: errors.mapped()
+      });
+   }
+};
--- src/controllers/products/categoryList.controller.js
+++ src/controllers/products/categoryList.controller.js
@@ -1,11 +1,11 @@
 const db = require("../../database/models")
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 module.exports = (req, res)=>{
-   const categoryId = req.params.categoryId
-    db.product.findByPk(categoryId,{
-        include: ["category"]
+    db.category.findAll({
+        include: ["products"]
     })  
-    .then((products) => {
-        res.render("products/categoryProduct", {products, toThousand})
+    
+    .then((categories) => {
+        res.render("products/categoryProduct", {categories, toThousand})
     })
 }
